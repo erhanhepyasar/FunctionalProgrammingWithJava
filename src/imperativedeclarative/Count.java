d5fb721d115f31560639be422f49eb9eac5b53a9
Erhan HEPYAŞAR (GM Bilgi Sistemleri) <GMBIM45@yurticikargo.local>
2019-12-22 10:26:38 +0300

    Initial commit

---
+++ src/imperativedeclarative/Count.java
@@ -0,0 +1,32 @@
+package imperativedeclarative;
+
+import java.util.stream.IntStream;
+
+public class Count {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Imperative - How style of programming
+		 */
+		
+		int sum1 = 0;
+		for (int i = 0; i <= 100; i++) {
+			sum1 += i;
+		}
+		
+		System.out.println("Sum using Imperative Approach: " + sum1);
+		
+		/*
+		 * Declarative - What style of programming
+		 */
+		
+		int sum2 = IntStream.rangeClosed(0, 100)
+				.parallel()		// Optional. Makes multi-threaded, splits the values
+				.sum();
+		
+		System.out.println("Sum using Declarative Approach: " + sum2);
+		
+	}
+
+}
